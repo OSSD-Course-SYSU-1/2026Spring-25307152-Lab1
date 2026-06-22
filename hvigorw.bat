a6a4237ec258f6c16248f9103a3523238f6984c8
tzhu66 <3392607422@qq.com>
2026-06-22 23:27:01 +0800

    Update config files: hvigorw.bat

--- hvigorw.bat
+++ hvigorw.bat
@@ -4,10 +4,13 @@ set HVIGOR_VERSION=4.0.2
 set HVIGOR_APP_HOME=%~dp0
 set HVIGOR_WRAPPER_DIR=%HVIGOR_APP_HOME%hvigor
 set HVIGOR_WRAPPER_JAR=%HVIGOR_WRAPPER_DIR%\hvigor-wrapper.jar
+
 if exist "%HVIGOR_WRAPPER_JAR%" goto execute
+
 echo ERROR: HVIGOR_WRAPPER_JAR not found.
 echo Please sync project in DevEco Studio to download hvigor wrapper.
 goto end
+
 :execute
 node "%HVIGOR_WRAPPER_DIR%\hvigor-wrapper.js" %*
 :end
